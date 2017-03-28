538be0e137e3e9a0917b517b1bccd598decb4030
pawel <kolakpk@gmail.com>
2017-03-28 15:06:54 +0200

    3-tier architecture

---
+++ TaskMaster/TaskMasterWebAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TaskMasterWebAPI.Startup))]
+
+namespace TaskMasterWebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
