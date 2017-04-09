0a3ec2439bee769e6289aeeaf219e18b1fdabe09
MalakayPL <218440@student.pwr.edu.pl>
2017-04-09 02:12:18 +0200

    Icons everywhere(almost)+ History list(whole)

--- TaskMaster/TaskMaster/TaskMaster/Pages/HistoryPage.xaml.cs
+++ TaskMaster/TaskMaster/TaskMaster/Pages/HistoryPage.xaml.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
-
+using TaskMaster.Models;
+using TaskMaster.Pages;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using XamForms.Controls;
+
 
 namespace TaskMaster
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+	
 	public partial class HistoryPage : ContentPage
 	{
 		public HistoryPage ()
 		{
 			InitializeComponent();
-		}
+            ListInitiate();
+        }
 
 	    private async void MainPageItem_OnClicked(object sender, EventArgs e)
 	    {
@@ -26,5 +28,41 @@ namespace TaskMaster
 	    {
 	        await Navigation.PushModalAsync(new CalendarPage());
 	    }
-	}
+        protected override void OnAppearing()
+        {
+            ListInitiate();
+        }
+
+        private async void ListInitiate()
+        {
+            var result = await App.Database.GetActivitiesByStatus(StatusType.Stop);
+            List<CustomList> dayPlan = new List<CustomList>();
+            foreach (var activity in result)
+            {
+                long time = 0;
+                var parts = await App.Database.GetPartsOfActivityByActivityId(activity.ActivityId);
+                foreach (var part in parts)
+                {
+                    time += long.Parse(part.Duration);
+                }
+                var task = await App.Database.GetTaskById(activity.TaskId);
+                var element = new CustomList()
+                {
+                    Name = task.Name,
+                    Description = task.Description,
+                    Time = time.ToString()
+                };
+                dayPlan.Add(element);
+            }
+           
+            DayPlan.ItemsSource = dayPlan;
+        }
+        public struct CustomList
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Time { get; set; }
+        }
+    }
 }
+
