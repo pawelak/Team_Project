f2706b8ebe740a680764efdfbf7a9b5c9017cf62
piotrekas1 <piotrekas1@gmail.com>
2017-04-12 11:39:10 +0200

    Login page, not working

--- TaskMaster/TaskMaster/TaskMaster/App.xaml.cs
+++ TaskMaster/TaskMaster/TaskMaster/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using TaskMaster.Pages;
 using Xamarin.Forms;
+using Xamarin.Forms.OAuth;
 
 namespace TaskMaster
 {
@@ -21,7 +23,9 @@ namespace TaskMaster
 	    } 
 		public App ()
 		{
-			InitializeComponent();
+		    OAuthAuthenticator.AddPRovider(OAuthProviders.Facebook("647866935403018"));
+		    OAuthAuthenticator.AddPRovider(OAuthProviders.Google("723494873981-qsnnp5vsa72f4d74bo8m8kqfsrbo25cq.apps.googleusercontent.com", "urn:ietf:wg:oauth:2.0:oob"));
+            InitializeComponent();
 			MainPage = new NavigationPage(new MainPage());
             /*var user = new User()
             {
---
+++ TaskMaster/TaskMaster/TaskMaster/Pages/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.OAuth;
+namespace TaskMaster.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+	    private async void Google_OnClicked(object sender, EventArgs e)
+	    {
+	        var authenticationResult = await OAuthAuthenticator.Authenticate();
+        }
+
+	    private void Facebook_OnClicked(object sender, EventArgs e)
+	    {
+	        throw new NotImplementedException();
+	    }
+	}
+}
