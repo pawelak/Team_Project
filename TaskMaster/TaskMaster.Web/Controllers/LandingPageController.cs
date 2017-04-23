910874d5c6825b681697b45229ff5c429b22b286
Bartek <Bartek@BARTEK-KOMPUTER>
2017-04-24 00:37:17 +0100

    Add basic registration and logging panels.

--- TaskMaster/TaskMaster.Web/Controllers/LandingPageController.cs
+++ TaskMaster/TaskMaster.Web/Controllers/LandingPageController.cs
@@ -3,16 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using Microsoft.Ajax.Utilities;
+using TaskMaster.Web.Models;
 
 namespace TaskMaster.Web.Controllers
 {
     public class LandingPageController : Controller
     {
         // GET: LandingPage
-        public ActionResult Index()
+        public ActionResult Home()
         {
 
             return View();
+        }
+
+      
+        [HttpPost]
+        public ActionResult Home(UserAccount user)
+        {
+            using (UsersDataBase db = new UsersDataBase())
+            {
+                var usr = db.UserAccounts.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserId"] = usr.UserId.ToString();
+                    Session["Email"] = usr.Email.ToString();
+                    
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email lub hasło jest niepoprawne.");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (UsersDataBase db = new UsersDataBase())
+                {
+                    db.UserAccounts.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + " poprawnie zarejstrowany."; //dodać opoxnienie i przekierowanie
+            }
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if ( 1 == 1 )
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Home");
+            }
         }
     }
 }
