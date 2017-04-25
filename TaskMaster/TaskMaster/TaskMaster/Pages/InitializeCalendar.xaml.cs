aebd302b0da79da2c82d4e90327d0bc51aef7beb
piotrekas1 <piotrekas1@gmail.com>
2017-04-25 16:22:54 +0200

    Working calendar

--- TaskMaster/TaskMaster/TaskMaster/Pages/InitializeCalendar.xaml.cs
+++ TaskMaster/TaskMaster/TaskMaster/Pages/InitializeCalendar.xaml.cs
@@ -11,58 +11,55 @@ namespace TaskMaster.Pages
         private readonly UserService _userService = new UserService();
         private readonly Calendar _calendar;
         private List<PartsOfActivityDto> _parts;
+
         public InitializeCalendar()
         {
-            try
+            var msg = new Label
             {
-                var msg = new Label
+                Text = "Wybierz Dzień",
+                FontSize = 25,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            _calendar = new Calendar
+            {
+                MultiSelectDates = false,
+                DisableAllDates = false,
+                WeekdaysShow = true,
+                ShowNumberOfWeek = true,
+                ShowNumOfMonths = 1,
+                EnableTitleMonthYearView = true,
+                WeekdaysTextColor = Color.Teal,
+                StartDay = DayOfWeek.Monday,
+                SelectedTextColor = Color.Fuchsia,
+            };
+            _calendar.DateClicked += (sender, e) =>
+            {
+                Navigation.PushModalAsync(new CalendarDayListPage(_calendar.SelectedDates[0]));
+            };
+            var vm = new CalendarVm();
+            _calendar.SetBinding(Calendar.DateCommandProperty, nameof(vm.DateChosen));
+            _calendar.SetBinding(Calendar.SelectedDateProperty, nameof(vm.Date));
+            _calendar.BindingContext = vm;
+            FillCalendar();
+            Navigation.PushModalAsync(new NavigationPage(new CalendarPage
+            {
+                BackgroundColor = Color.White,
+                Content = new ScrollView
                 {
-                    Text = "Wybierz Dzień",
-                    FontSize = 25,
-                    HorizontalOptions = LayoutOptions.Center
-                };
-                _calendar = new Calendar
-                {
-                    MultiSelectDates = false,
-                    DisableAllDates = false,
-                    WeekdaysShow = true,
-                    ShowNumberOfWeek = true,
-                    ShowNumOfMonths = 1,
-                    EnableTitleMonthYearView = true,
-                    WeekdaysTextColor = Color.Teal,
-                    StartDay = DayOfWeek.Monday,
-                    SelectedTextColor = Color.Fuchsia,
-                };
-                _calendar.DateClicked += (sender, e) => {
-                    Navigation.PushModalAsync(new CalendarDayListPage(_calendar.SelectedDates[0]));
-                };
-                var vm = new CalendarVm();
-                _calendar.SetBinding(Calendar.DateCommandProperty, nameof(vm.DateChosen));
-                _calendar.SetBinding(Calendar.SelectedDateProperty, nameof(vm.Date));
-                _calendar.BindingContext = vm;
-                FillCalendar();
-                Navigation.PushModalAsync(new NavigationPage(new CalendarPage
-                {
-                    BackgroundColor = Color.White,
-                    Content = new ScrollView
+                    Content = new StackLayout
                     {
-                        Content = new StackLayout
+                        Padding = new Thickness(5, Device.OS == TargetPlatform.iOS ? 25 : 5, 5, 5),
+                        VerticalOptions = LayoutOptions.Center,
+                        Children =
                         {
-                            Padding = new Thickness(5, Device.OS == TargetPlatform.iOS ? 25 : 5, 5, 5),
-                            VerticalOptions = LayoutOptions.Center,
-                            Children = {
-                                msg, _calendar
-                            }
+                            msg,
+                            _calendar
                         }
                     }
-                }));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+                }
+            }));
         }
+
         protected override async void OnDisappearing()
         {
             await Navigation.PushModalAsync(new NavigationPage(new MainPage()));
@@ -72,7 +69,7 @@ namespace TaskMaster.Pages
             _parts = await _userService.GetPartsOfActivityList();
             foreach (var p in _parts)
             {
-                _calendar.SpecialDates.Add(new SpecialDate(Convert.ToDateTime(p.Start)) { BackgroundColor = Color.Green, TextColor = Color.Black, BorderColor = Color.Blue, BorderWidth = 8, Selectable = true });
+                _calendar.SpecialDates.Add(new SpecialDate(DateTime.ParseExact(p.Start, "HH:mm:ss dd/MM/yyyy", null)) { BackgroundColor = Color.Green, TextColor = Color.Black, BorderColor = Color.Blue, BorderWidth = 8, Selectable = true });
             }
             _calendar.RaiseSpecialDatesChanged();//refresh
         }
