d62f7a5b71ce4a0c166d04f6a2eed5ec0060cf8f
piotrekas1 <piotrekas1@gmail.com>
2017-04-29 19:56:32 +0200

    tries to make login work

--- TaskMaster/TaskMaster/TaskMaster/Pages/LoginPage.xaml.cs
+++ TaskMaster/TaskMaster/TaskMaster/Pages/LoginPage.xaml.cs
@@ -1,23 +1,77 @@
 ﻿using System;
+using System.Text;
 using Xamarin.Forms.Xaml;
+using Xamarin.Auth.XamarinForms;
 namespace TaskMaster.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage
 	{
-		public LoginPage ()
+        public LoginPage ()
 		{
 			InitializeComponent ();
 		}
 
 	    private async void Google_OnClicked(object sender, EventArgs e)
 	    {
-	        await Navigation.PushModalAsync(new ProviderLoginPage("Google"));
-        }
+	        try
+	        {
+	            var oauth = new OAuthProviderSetting();
+	            var auth = oauth.LoginWithProvider("Google");
+	            auth.Completed += (s, ea) =>
+	            {
+	                StringBuilder sb = new StringBuilder();
+	                if (ea.Account?.Properties != null)
+	                {
+	                    sb.Append("Token = ").AppendLine($"{ea.Account.Properties["access_token"]}");
+	                }
+	                else
+	                {
+	                    sb.Append("Not authenticated ").AppendLine("Account.Properties does not exist");
+	                }
+	                DisplayAlert
+	                (
+	                    "Authentication Results",
+	                    sb.ToString(),
+	                    "OK"
+	                );
+	            };
 
-	    private async void Facebook_OnClicked(object sender, EventArgs e)
+	            auth.Error += (s, ea) =>
+	            {
+	                var sb = new StringBuilder();
+	                sb.Append("Error = ").AppendLine($"{ea.Message}");
+	                DisplayAlert
+	                (
+	                    "Authentication Error",
+	                    sb.ToString(),
+	                    "OK"
+	                );
+	            };
+	            AuthenticationState.Authenticator = auth;
+	            if (true)
+	            {
+	                // Renderers Implementaion
+	                await Navigation.PushModalAsync(new AuthenticatorPage());
+	            }
+	            else
+	            {
+	                // Presenters Implementation
+	                Xamarin.Auth.Presenters.OAuthLoginPresenter presenter = null;
+	                presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
+	                presenter.Login(auth);
+	            }
+	        }
+	        catch (Exception exception)
+	        {
+	            await DisplayAlert("e", exception.Message, "OK");
+	            throw;
+	        }
+	    }
+
+	    private void Facebook_OnClicked(object sender, EventArgs e)
 	    {
-	        await Navigation.PushModalAsync(new ProviderLoginPage("FaceBook"));
+	        //await Navigation.PushModalAsync(new ProviderLoginPage("FaceBook"));
         }
 	}
 }
