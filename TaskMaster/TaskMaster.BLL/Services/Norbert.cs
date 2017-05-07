246f729461b5de61cfe03d35266111f1ddb153a4
Norbert <Norbert@DESKTOP-L962ICA>
2017-05-07 18:17:55 +0200

    Database : 07.05.17

--- TaskMaster/TaskMaster.BLL/Services/Norbert.cs
+++ TaskMaster/TaskMaster.BLL/Services/Norbert.cs
@@ -10,8 +10,9 @@ namespace TaskMaster.BLL.Services
 {
     public class Norbert
     {
+        public ActivityRepositories ActivityRepositories = new ActivityRepositories();
+        public GroupRepositories GroupRepositories = new GroupRepositories();
         public UserRepositories UserRepositories = new UserRepositories();
-
 
         public bool IsEmailInBase(string email)
         {
@@ -19,18 +20,32 @@ namespace TaskMaster.BLL.Services
             return list.Any(var => var.Email.Equals(email));
         }
 
-        //public List<ActivityDto> FromTime(DateTime start, DateTime stop)
-        //{
-        //    return 
-        //}
+        public bool IsNameInBase(string name)
+        {
+            var list = UserRepositories.GetAll();
+            return list.Any(var => var.Name.Equals(name));
+        }
 
-        //grupy:
-        //wyszukiwanie urzytkowników po emailu i nazwie // wszystkich z grupy?
-        //pobieranie czonków grupy
+        public List<UserDto> UsersInGroup(string groupname)
+        {
+            var group = GroupRepositories.GetAll().FirstOrDefault(var => var.NameGroup.Equals(groupname));
+            var list = @group.UserGroup.Select(VARIABLE => VARIABLE.User).ToList();
+            return list;
+        }
+
+        public List<ActivityDto> FromTime(DateTime start, DateTime stop)
+        {
+            var list2 = new List<ActivityDto>();
+            var list = ActivityRepositories.GetAll();
+            foreach (var VAR1 in list)
+            {
+                list2.AddRange(from VAR2 in VAR1.PartsOfActivity where VAR2.Start.CompareTo(start) > 0 where VAR2.Start.CompareTo(stop) < 0 where VAR2.Stop.CompareTo(start) > 0 where VAR2.Stop.CompareTo(stop) < 0 select VAR1);
+            }
+            return list2;
+        }
 
 
-        //    activity i partsof:
-        //pobierz z określonych dat - o ile się dat
+
     }
 
 }
