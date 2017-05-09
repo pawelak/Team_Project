48b9cfa75a1c08db061b8c25f10f63240a1b4332
piotrekas1 <piotrekas1@gmail.com>
2017-05-09 14:29:56 +0200

    Changed api to 24

--- TaskMaster/TaskMaster/TaskMaster.Android/MainActivity.cs
+++ TaskMaster/TaskMaster/TaskMaster.Android/MainActivity.cs
@@ -34,91 +34,6 @@ namespace TaskMaster.Droid
                 }
                 CrossLocalNotifications.Current.Show(task.Name, "Za 5 minut", part.PartId, DateTime.Parse(part.Start).AddMinutes(-5));
             }
-            /*var started = await _userService.GetActivitiesByStatus(StatusType.Start);
-            foreach (var start in started)
-            {
-                var alert = new AlertDialog.Builder(this);
-                if (start.TaskId == 0)
-                {
-                    var noTask = new TasksDto
-                    {
-                        Name = "Unnamed Activity " + start.ActivityId
-                    };
-                    alert.SetTitle("Masz niezapauzowaną aktywność " + noTask.Name);
-                }
-                else
-                {
-                    var task = await _userService.GetTaskById(start.TaskId);
-                    alert.SetTitle("Masz niezapauzowaną aktywność " + task.Name);
-                }
-                alert.SetMessage("Czy była ona aktywna od zamknięcia aplikacji? \n" +
-                                 "Jeżeli wybierzesz nie, czas aktywności może być niewłaściwy \n" +
-                                 "Jeżeli wybierzesz tak, czas końca aktywności będzie czasem zatwierdzenia tego komunikatu");
-                alert.SetNegativeButton("Nie", (s, a) =>
-                {
-                    var alertNo = new AlertDialog.Builder(this);
-                    alertNo.SetTitle("Kontynuacja");
-                    alertNo.SetMessage("Czy chcesz żeby aktywność była kontynuowana?");
-                    alertNo.SetPositiveButton("Tak", async (s1, a1) =>
-                    {
-                        var part2 = new PartsOfActivityDto
-                        {
-                            Start = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"),
-                            ActivityId = start.ActivityId,
-                            Duration = "0"
-                        };
-                        part2.PartId = await _userService.SavePartOfActivity(part2);
-                        var sw = new Stopwatch();
-                        var stopwatch = new Stopwatches(sw, part2.PartId);
-                        App.Stopwatches.Add(stopwatch);
-                        App.Stopwatches[App.Stopwatches.Count - 1].Start();
-                        await _userService.SaveActivity(start);
-                    });
-                    alertNo.SetNegativeButton("Nie", async (s1, a1) =>
-                    {
-                        start.Status = StatusType.Pause;
-                        await _userService.SaveActivity(start);
-                    });
-                    var dialogNo = alertNo.Create();
-                    dialogNo.Show();
-                });
-                alert.SetPositiveButton("Tak", async (s, a) =>
-                {
-                    var part = await _userService.GetLastActivityPart(start.ActivityId);
-                    part.Stop = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy");
-                    var time = (long)(DateTime.Now - DateTime.ParseExact(part.Start, "HH:mm:ss dd/MM/yyyy", null)).TotalMilliseconds;
-                    part.Duration = time.ToString();
-                    await _userService.SaveActivity(start);
-                    await _userService.SavePartOfActivity(part);
-                    var alertOk = new AlertDialog.Builder(this);
-                    alertOk.SetTitle("Kontynuacja");
-                    alertOk.SetMessage("Czy chcesz żeby aktywność była kontynuowana?");
-                    alertOk.SetPositiveButton("Tak", async (s1, a1) =>
-                    {
-                        var part3 = new PartsOfActivityDto
-                        {
-                            Start = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"),
-                            ActivityId = start.ActivityId,
-                            Duration = "0"
-                        };
-                        part3.PartId = await _userService.SavePartOfActivity(part3);
-                        var sw2 = new Stopwatch();
-                        var stopwatch2 = new Stopwatches(sw2, part3.PartId);
-                        App.Stopwatches.Add(stopwatch2);
-                        App.Stopwatches[App.Stopwatches.Count - 1].Start();
-                    });
-                    alertOk.SetNegativeButton("Nie", async (s1, a1) =>
-                    {
-                        start.Status = StatusType.Pause;
-                        await _userService.SaveActivity(start);
-                    });
-                    var dialog = alertOk.Create();
-                    dialog.Show();
-                });
-                var dialogOk = alert.Create();
-                dialogOk.Show();
-            }*/
-            Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
             XamForms.Controls.Droid.Calendar.Init();
             LoadApplication(new App());
         }
@@ -163,7 +78,6 @@ namespace TaskMaster.Droid
                 item.Restart();
             }
         }
-
     }
 }
 
--- TaskMaster/TaskMaster/TaskMaster/Pages/LoginPage.xaml.cs
+++ TaskMaster/TaskMaster/TaskMaster/Pages/LoginPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Text;
 using Xamarin.Forms.Xaml;
-using Xamarin.Auth.XamarinForms;
+using Xamarin.Forms;
+using TaskMaster.Droid;
 namespace TaskMaster.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -12,63 +12,13 @@ namespace TaskMaster.Pages
 			InitializeComponent ();
 		}
 
-	    private async void Google_OnClicked(object sender, EventArgs e)
+	    private void Google_OnClicked(object sender, EventArgs e)
 	    {
-	        try
+	        if (Device.RuntimePlatform == "Android")
 	        {
-	            var oauth = new OAuthProviderSetting();
-	            var auth = oauth.LoginWithProvider("Google");
-	            auth.Completed += (s, ea) =>
-	            {
-	                StringBuilder sb = new StringBuilder();
-	                if (ea.Account?.Properties != null)
-	                {
-	                    sb.Append("Token = ").AppendLine($"{ea.Account.Properties["access_token"]}");
-	                }
-	                else
-	                {
-	                    sb.Append("Not authenticated ").AppendLine("Account.Properties does not exist");
-	                }
-	                DisplayAlert
-	                (
-	                    "Authentication Results",
-	                    sb.ToString(),
-	                    "OK"
-	                );
-	            };
-
-	            auth.Error += (s, ea) =>
-	            {
-	                var sb = new StringBuilder();
-	                sb.Append("Error = ").AppendLine($"{ea.Message}");
-	                DisplayAlert
-	                (
-	                    "Authentication Error",
-	                    sb.ToString(),
-	                    "OK"
-	                );
-	            };
-	            AuthenticationState.Authenticator = auth;
-	            if (true)
-	            {
-	                // Renderers Implementation
-	                await Navigation.PushModalAsync(new AuthenticatorPage()
-	                {
-	                    Authenticator = auth
-	                });
-	            }
-	            else
-	            {
-	                // Presenters Implementation
-	                Xamarin.Auth.Presenters.OAuthLoginPresenter presenter = null;
-	                presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
-	                presenter.Login(auth);
-	            }
 	        }
-	        catch (Exception exception)
+	        else if (Device.RuntimePlatform == "iOS")
 	        {
-	            await DisplayAlert("e", exception.Message, "OK");
-	            throw;
 	        }
 	    }
 
