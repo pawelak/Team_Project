4fbb76d53ca00ab5f1b19a8f3dcbcd4c99cbff6b
Martyna Łagożna <m.lagozna@live.com>
2017-05-15 00:14:26 +0200

    history page with list of activities added

---
+++ TaskMaster/TaskMaster.Web/Controllers/HistoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskMaster.Web.Controllers
+{
+    public class HistoryController : Controller
+    {
+        // GET: History
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
